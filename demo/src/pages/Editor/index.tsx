66e60501a9b9656c7e9271fb9c3e0818c7d2a070
deepak4499 <40885656+deepak4499@users.noreply.github.com>
2024-09-26 18:25:07 +0530

    Topband and Editor index changes

--- demo/src/pages/Editor/index.tsx
+++ demo/src/pages/Editor/index.tsx
@@ -1,61 +1,41 @@
-/* eslint-disable react/jsx-wrap-multilines */
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import template from '@demo/store/template';
 import { useAppSelector } from '@demo/hooks/useAppSelector';
 import { useLoading } from '@demo/hooks/useLoading';
-import {
-  Button,
-  ConfigProvider,
-  Dropdown,
-  Menu,
-  Message,
-  PageHeader,
-  Select,
-} from '@arco-design/web-react';
 import { useQuery } from '@demo/hooks/useQuery';
 import { useHistory } from 'react-router-dom';
-import { cloneDeep, set, isEqual } from 'lodash';
+import { cloneDeep, set } from 'lodash';
 import { Loading } from '@demo/components/loading';
 import mjml from 'mjml-browser';
 import services from '@demo/services';
-import { IconMoonFill, IconSunFill } from '@arco-design/web-react/icon';
 import { Liquid } from 'liquidjs';
 import { saveAs } from 'file-saver';
 import {
-  BlockAvatarWrapper,
+  ToolsPanel,
   EmailEditor,
   EmailEditorProvider,
   EmailEditorProviderProps,
-  IEmailTemplate,
+  IEmailTemplate
 } from 'easy-email-editor';
+import './App.scss';
+
+import closeIconImg from './assets/close-icon.svg';
 
 import { Stack } from '@demo/components/Stack';
 import { pushEvent } from '@demo/utils/pushEvent';
-import { FormApi } from 'final-form';
 import { UserStorage } from '@demo/utils/user-storage';
 
 import { AdvancedType, IBlockData, JsonToMjml } from 'easy-email-core';
-import { ExtensionProps, MjmlToJson, StandardLayout } from 'easy-email-extensions';
-import { AutoSaveAndRestoreEmail } from '@demo/components/AutoSaveAndRestoreEmail';
+import { ExtensionProps, StandardLayout } from 'easy-email-extensions';
 
-// Register external blocks
-import './components/CustomBlocks';
+import { AutoSaveAndRestoreEmail } from '@demo/components/AutoSaveAndRestoreEmail';
 
 import 'easy-email-editor/lib/style.css';
 import 'easy-email-extensions/lib/style.css';
 import blueTheme from '@arco-themes/react-easy-email-theme/css/arco.css?inline';
-import purpleTheme from '@arco-themes/react-easy-email-theme-purple/css/arco.css?inline';
-import greenTheme from '@arco-themes/react-easy-email-theme-green/css/arco.css?inline';
 import { testMergeTags } from './testMergeTags';
 import { useMergeTagsModal } from './components/useMergeTagsModal';
-
-import { useWindowSize } from 'react-use';
-import { CustomBlocksType } from './components/CustomBlocks/constants';
-import { Uploader } from '@demo/utils/Uploader';
-import enUS from '@arco-design/web-react/es/locale/en-US';
-
-import { useShowCommercialEditor } from '@demo/hooks/useShowCommercialEditor';
 
 const defaultCategories: ExtensionProps['categories'] = [
   {
@@ -117,45 +97,7 @@ const defaultCategories: ExtensionProps['categories'] = [
         payload: [['25%', '25%', '25%', '25%']],
       },
     ],
-  },
-  {
-    label: 'Custom',
-    active: true,
-    displayType: 'custom',
-    blocks: [
-      <BlockAvatarWrapper type={CustomBlocksType.PRODUCT_RECOMMENDATION}>
-        <div
-          style={{
-            position: 'relative',
-            border: '1px solid #ccc',
-            marginBottom: 20,
-            width: '80%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-          }}
-        >
-          <img
-            src={
-              'http://res.cloudinary.com/dwkp0e1yo/image/upload/v1665841389/ctbjtig27parugrztdhk.png'
-            }
-            style={{
-              maxWidth: '100%',
-            }}
-          />
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              zIndex: 2,
-            }}
-          />
-        </div>
-      </BlockAvatarWrapper>,
-    ],
-  },
+  }
 ];
 
 const fontList = [
@@ -167,18 +109,9 @@ const fontList = [
   'Georgia',
   'Lato',
   'Montserrat',
-  '黑体',
-  '仿宋',
-  '楷体',
-  '标楷体',
-  '华文仿宋',
-  '华文楷体',
-  '宋体',
-  '微软雅黑',
 ].map(item => ({ value: item, label: item }));
 
 export default function Editor() {
-  const { featureEnabled } = useShowCommercialEditor();
   const dispatch = useDispatch();
   const history = useHistory();
   const templateData = useAppSelector('template');
@@ -217,77 +150,6 @@ export default function Editor() {
     });
   }, []);
 
-  const onImportMJML = async ({
-    restart,
-  }: {
-    restart: (val: IEmailTemplate) => void;
-  }) => {
-    const uploader = new Uploader(() => Promise.resolve(''), {
-      accept: 'text/mjml',
-      limit: 1,
-    });
-
-    const [file] = await uploader.chooseFile();
-    const reader = new FileReader();
-    const pageData = await new Promise<[string, IEmailTemplate['content']]>(
-      (resolve, reject) => {
-        reader.onload = function (evt) {
-          if (!evt.target) {
-            reject();
-            return;
-          }
-          try {
-            const pageData = MjmlToJson(evt.target.result as any);
-            resolve([file.name, pageData]);
-          } catch (error) {
-            reject();
-          }
-        };
-        reader.readAsText(file);
-      },
-    );
-
-    restart({
-      subject: pageData[0],
-      content: pageData[1],
-      subTitle: '',
-    });
-  };
-
-  const onImportJSON = async ({
-    restart,
-  }: {
-    restart: (val: IEmailTemplate) => void;
-  }) => {
-    const uploader = new Uploader(() => Promise.resolve(''), {
-      accept: 'application/json',
-      limit: 1,
-    });
-
-    const [file] = await uploader.chooseFile();
-    const reader = new FileReader();
-    const emailTemplate = await new Promise<IEmailTemplate>((resolve, reject) => {
-      reader.onload = function (evt) {
-        if (!evt.target) {
-          reject();
-          return;
-        }
-        try {
-          const template = JSON.parse(evt.target.result as any) as IEmailTemplate;
-          resolve(template);
-        } catch (error) {
-          reject();
-        }
-      };
-      reader.readAsText(file);
-    });
-
-    restart({
-      subject: emailTemplate.subject,
-      content: emailTemplate.content,
-      subTitle: emailTemplate.subTitle,
-    });
-  };
 
   const onExportMJML = (values: IEmailTemplate) => {
     const mjmlString = JsonToMjml({
@@ -317,13 +179,6 @@ export default function Editor() {
     saveAs(new Blob([html], { type: 'text/html' }), 'easy-email.html');
   };
 
-  const onExportJSON = (values: IEmailTemplate) => {
-    navigator.clipboard.writeText(JSON.stringify(values, null, 2));
-    saveAs(
-      new Blob([JSON.stringify(values, null, 2)], { type: 'application/json' }),
-      'easy-email.json',
-    );
-  };
 
   const initialValues: IEmailTemplate | null = useMemo(() => {
     if (!templateData) return null;
@@ -361,12 +216,12 @@ export default function Editor() {
   if (!initialValues) return null;
 
   return (
-    <ConfigProvider locale={enUS}>
+    <div>
       <div>
         <style>{blueTheme}</style>
         <EmailEditorProvider
           key={id}
-          height={featureEnabled ? 'calc(100vh - 108px)' : 'calc(100vh - 68px)'}
+          height={'calc(100vh - 72px)'}
           data={initialValues}
           onUploadImage={onUploadImage}
           fontList={fontList}
@@ -381,75 +236,34 @@ export default function Editor() {
           {({ values }, { submit, restart }) => {
             return (
               <>
-                <PageHeader
+                <div
+                  className="page-header"
                   style={{ background: 'var(--color-bg-2)' }}
-                  backIcon
-                  title='Edit'
-                  onBack={() => history.push('/')}
-                  extra={
-                    <Stack alignment='center'>
-                      <Dropdown
-                        droplist={
-                          <Menu>
-                            <Menu.Item
-                              key='MJML'
-                              onClick={() => onImportMJML({ restart })}
-                            >
-                              Import from MJML
-                            </Menu.Item>
-
-                            <Menu.Item
-                              key='JSON'
-                              onClick={() => onImportJSON({ restart })}
-                            >
-                              Import from JSON
-                            </Menu.Item>
-                          </Menu>
-                        }
-                      >
-                        <Button>
-                          <strong>Import</strong>
-                        </Button>
-                      </Dropdown>
-
-                      <Dropdown
-                        droplist={
-                          <Menu>
-                            <Menu.Item
-                              key='Export MJML'
-                              onClick={() => onExportMJML(values)}
-                            >
-                              Export MJML
-                            </Menu.Item>
-                            <Menu.Item
-                              key='Export HTML'
-                              onClick={() => onExportHTML(values)}
-                            >
-                              Export HTML
-                            </Menu.Item>
-                            <Menu.Item
-                              key='Export JSON'
-                              onClick={() => onExportJSON(values)}
-                            >
-                              Export JSON
-                            </Menu.Item>
-                          </Menu>
-                        }
-                      >
-                        <Button>
-                          <strong>Export</strong>
-                        </Button>
-                      </Dropdown>
-                      <Button
-                        type='primary'
-                        target='_blank'
-                        href='https://demo.easyemail.pro?utm_source=easyemail'
-                      >
-                        Try commercial version
-                      </Button>
-                    </Stack>
-                  }
-                />
+                >
+                  <div className="d-flex">
+                    <h2>Invoice Notification</h2>
+                  </div>
+                  <Stack alignment='center'>
+                    <ToolsPanel />
+                    <div
+                      className="btn-primary"
+                      key='Export MJML'
+                      onClick={() => onExportMJML(values)}
+                    >
+                      Export MJML
+                    </div>
+                    <div
+                      className="btn-primary"
+                      key='Export HTML'
+                      onClick={() => onExportHTML(values)}
+                    >
+                      Export HTML
+                    </div>
+                    <div onClick={() => history.push('/')}>
+                      <img src={closeIconImg} alt="Back" height="15" width="15" style={{ verticalAlign: 'middle', cursor: 'pointer' }} />
+                    </div>
+                  </Stack>
+                </div>
 
                 <StandardLayout categories={defaultCategories}>
                   <EmailEditor />
@@ -460,6 +274,6 @@ export default function Editor() {
           }}
         </EmailEditorProvider>
       </div>
-    </ConfigProvider>
+    </div>
   );
 }
